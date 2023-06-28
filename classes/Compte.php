5ab6ed2e92f3f99f9e234e33d6912d60d2517575
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-28 21:29:13 +0200

    feat(Compte): add magic constructor for Compte class

--- classes/Compte.php
+++ classes/Compte.php
@@ -19,4 +19,20 @@ class Compte // Déclaration de l'objet
      */
     public $solde;
 
+    // Fonction magique qui est le constructeur de notre fonction
+    /**
+     * Constructeur du compte bancaire
+     *
+     * @param string $nom Nom du titulaire
+     * @param float $montant Montant du solde à l'ouverture
+     */
+    public function __construct(string $nom, float $montant = 100) 
+    {
+        // On attribut le nom à la propriété titulaire de l'instance créée.
+        $this->titulaire = $nom; // On injecte le nom dans la propriété "titulaire"
+
+        // On attribut le montant à la propriété solde
+        $this->solde = $montant;
+    }
+
 }
