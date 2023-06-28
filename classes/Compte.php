5ed9d1c83a926e7478cb09c8fe44ba1491a0e8ad
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-28 21:10:56 +0200

    feat(Compte): add Compte class with titulaire and solde properties

---
+++ classes/Compte.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Objet Compte bancaire
+ */
+// Déclaration de ma classe "Compte"
+class Compte // Déclaration de l'objet
+{
+    // Propriétés
+    /**
+     * Titulaire du compte
+     *
+     * @var string
+     */
+    public $titulaire;
+    /**
+     * Solde du compte
+     *
+     * @var float
+     */
+    public $solde;
+
+}
