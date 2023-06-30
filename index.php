525a3a36ced80c63db33ccab79e4464ee5ad4dc4
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-30 13:34:05 +0200

    test(index): add test for 'CompteEpargneCourant' class in CompteEpargneCourant.php

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 require_once "classes/Compte.php";
 require_once 'classes/CompteCourant.php';
 require_once 'classes/CompteEpargne.php';
+require_once 'classes/CompteEpargneCourant.php';
 
 // On instancie le compte
 $compte1 = new CompteCourant('Benoit', 500, 200);
-
 $compte1->retirer(200);
-
 var_dump($compte1);
 
-$compteEpargne = new CompteEpargne('Robert', 200, 10);
+$compteEpargne = new CompteEpargneCOurant('Robert', 200, 10, 200);
+
 var_dump($compteEpargne);
 
 $compteEpargne->verserInterets();
-
+$compteEpargne->retirer(300)
+;
 var_dump($compteEpargne);
