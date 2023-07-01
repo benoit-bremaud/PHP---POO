4250419413c455594f9e42312a8685ebd48c35e0
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-01 13:50:44 +0200

    refactor(index): optimize imports and update code in file

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-require_once "classes/Compte.php";
-require_once 'classes/CompteCourant.php';
-require_once 'classes/CompteEpargne.php';
-require_once 'classes/CompteEpargneCourant.php';
+use App\Client\Compte as CompteClient;
+use App\Banque\{CompteCourant, CompteEpargne, CompteEpargneCourant};
+// use App\Banque\CompteEpargne;
+
+require_once 'classes/Banque/Compte.php';
+require_once 'classes/Banque/CompteCourant.php';
+require_once 'classes/Banque/CompteEpargne.php';
+require_once 'classes/Banque/CompteEpargneCourant.php';
+require_once 'classes/Client/Compte.php';
 
 // On instancie le compte
 $compte1 = new CompteCourant('Benoit', 500, 200);
 $compte1->retirer(200);
 var_dump($compte1);
 
-$compteEpargne = new CompteEpargneCOurant('Robert', 200, 10, 200);
+$compteEpargne = new CompteEpargneCourant('Robert', 200, 10, 200);
 
 var_dump($compteEpargne);
 
@@ -17,3 +22,8 @@ $compteEpargne->verserInterets();
 $compteEpargne->retirer(300)
 ;
 var_dump($compteEpargne);
+
+// $client = new App\Client\Compte;
+$client = new CompteClient;
+
+var_dump($client);
