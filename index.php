a3b23d498f150c3d5af2df4bbff1a46447a59189
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-02 23:05:04 +0200

    refactor(Index): modify code to use dependency injection for Compte instantiation

--- index.php
+++ index.php
@@ -2,23 +2,28 @@
 
 use App\Autoloader;
 use App\Client\Compte as CompteClient;
-use App\Banque\{CompteCourant, CompteEpargne, CompteEpargneCourant};
+use App\Banque\{CompteCourant, CompteEpargne};
 // use App\Banque\CompteEpargne;
 
 require_once 'classes/Autoloader.php';
 Autoloader::register();
 
+// On instancie le compte client
+$client = new CompteClient('Bremaud', 'Benoit', 'Cannes');
 
 // On instancie le compte
-$compte1 = new CompteCourant('Benoit', 500, 200);
+$compte1 = new CompteCourant($client, 500, 200);
 // $compte2 = new CompteClient();
 
 // $compte1->retirer(200);
 var_dump($compte1);
 
-// $compteEpargne = new CompteEpargneCourant('Robert', 200, 10, 200);
+$compteEpargne = new CompteEpargne($client, 200, 10, 200);
 
-// var_dump($compteEpargne);
+var_dump($compteEpargne);
+
+
+var_dump($client);
 
 // $compteEpargne->verserInterets();
 // $compteEpargne->retirer(300)
