bc5aa889076539487e63e04e5b1992b97e362a12
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-02 23:16:19 +0200

    refactor(CompteCourant): Compte instantiation with dependency injection

--- classes/Banque/CompteCourant.php
+++ classes/Banque/CompteCourant.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Banque;
 
+use App\Client\Compte as CompteClient;
+
 /**
  * Compte bancaire (hérite de la class Compte)
  */
@@ -11,14 +13,15 @@ class CompteCourant extends Compte
     /**
      * Constructeur de compte courant
      *
-     * @param string $nom Nom du titulaire
+     * @param CompteClient $compte Compte Client du titulaire
      * @param float $montant Montant du solde à l'ouverture
      * @param integer $decouvert Découvert autorisé
+     * @return void 
      */
-    public function __construct(string $nom, float $montant, int $decouvert)
+    public function __construct(CompteClient $compte, float $montant, int $decouvert)
     {
-        // On transfère les informations nécessaires au construction de Compte
-        parent::__construct($nom, $montant);
+        // On transfère les informations nécessaires au constructeur de Compte
+        parent::__construct($compte, $montant);
 
         $this->decouvert = $decouvert;
     }
