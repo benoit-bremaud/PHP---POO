e9a6c10a75c910a1884594943c073add2ee68c0f
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-02 21:49:06 +0200

    refactor(Index): refactor code to use Autoloader for class declarations

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
+
+use App\Autoloader;
 use App\Client\Compte as CompteClient;
 use App\Banque\{CompteCourant, CompteEpargne, CompteEpargneCourant};
 // use App\Banque\CompteEpargne;
 
-require_once 'classes/Banque/Compte.php';
-require_once 'classes/Banque/CompteCourant.php';
-require_once 'classes/Banque/CompteEpargne.php';
-require_once 'classes/Banque/CompteEpargneCourant.php';
-require_once 'classes/Client/Compte.php';
+require_once 'classes/Autoloader.php';
+Autoloader::register();
+
 
 // On instancie le compte
 $compte1 = new CompteCourant('Benoit', 500, 200);
-$compte1->retirer(200);
+// $compte2 = new CompteClient();
+
+// $compte1->retirer(200);
 var_dump($compte1);
 
-$compteEpargne = new CompteEpargneCourant('Robert', 200, 10, 200);
+// $compteEpargne = new CompteEpargneCourant('Robert', 200, 10, 200);
 
-var_dump($compteEpargne);
+// var_dump($compteEpargne);
 
-$compteEpargne->verserInterets();
-$compteEpargne->retirer(300)
-;
-var_dump($compteEpargne);
+// $compteEpargne->verserInterets();
+// $compteEpargne->retirer(300)
+// ;
+// var_dump($compteEpargne);
 
-// $client = new App\Client\Compte;
-$client = new CompteClient;
+// // $client = new App\Client\Compte;
+// $client = new CompteClient;
 
-var_dump($client);
+// var_dump($client);
